4784543bf6fe93feaa4391dce3c21a8cb9adf1d2
wwagner <wolfgang.wagner@itsv.at>
2025-02-04 16:55:06 +0100

    nonlinear

--- src/training/tryout.py
+++ src/training/tryout.py
@@ -1,25 +1,31 @@
 from pathlib import Path
-import shutil
+import math
+
+import matplotlib.pyplot as plt
 
 
 def main():
-    prefix = "A-SSS-X"
-    name = "A-SSS-X-999-hallo.2"
-    name1 = name[len(prefix) + 1 :]
-    print("--", prefix, name, name1)
+    work_dir = Path.home() / "tmp"
 
+    def func(x: float, g: float) -> float:
+        f = math.pow(10, g)
+        return math.pow(f, x - 1) + (math.pow(f, - 1) * (x - 1))
 
-def rename():
-    print("-- rename")
-    indir = Path.home() / "tmp" / "sumosim" / "results1"
-    outdir = Path.home() / "tmp" / "sumosim" / "results"
-    outdir.mkdir(parents=True, exist_ok=True)
-    for f in indir.iterdir():
-        if f.name.startswith("P"):
-            stem = f.name[1:]
-            new_name = f"Q0-{stem}"
-            target = outdir / new_name
-            print(f"{str(f.name):50s} ---> {target}")
-            if not target.exists():
-                shutil.copy(f, target)
-                print(f"copied to {target}")
+    dist = 100
+    n = 4
+    gs = [0, 0.5, 1, 1.5, 2]
+    x = list([a / float(dist - 1) for a in range(dist)])
+
+    x1 = [a / (n - 1) for a in range(n)]
+    for g in gs:
+        y1 = [func(x, g) for x in x1]
+        y1_str = ", ".join([f"{y:5.3f}" for y in y1])
+        print(f"{g:6.1f} {y1_str}")
+
+    fig, ax = plt.subplots(figsize=(15, 15))
+    for g in gs:
+        y1 = [func(a, g) for a in x]
+        ax.plot(x, y1)
+    out_path = work_dir / "a.png"
+    fig.savefig(out_path)
+    plt.close(fig)
